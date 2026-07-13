0d0351f312f0c7d8208eebd42440ff52a3a86219
Shashwat Mahajan <mahajanshashwat72@gmail.com>
2026-07-13 09:01:49 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+
 
 
 @dataclass
